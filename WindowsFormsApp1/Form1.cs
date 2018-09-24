ef1b320d05f0d1ecb913f69e01b78909cc7498e6
RC0030800 <RC0030800@CRO-24-15>
2018-09-24 15:45:32 +0100

    added try

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -82,11 +82,14 @@ namespace WindowsFormsApp1
 
         private async void button4_Click(object sender, EventArgs e)
         {
-            while (pictureBox1.Location.Y + 5 < ClientRectangle.Width - pictureBox1.Width)
+            try
             {
-                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 5);
-                await Task.Delay(Convert.ToInt32(textBox4.Text));
-            }
+                while (pictureBox1.Location.Y + 5 < ClientRectangle.Width - pictureBox1.Width)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 5);
+                    await Task.Delay(Convert.ToInt32(textBox4.Text));
+                    
+                }
         }
     }
 }
